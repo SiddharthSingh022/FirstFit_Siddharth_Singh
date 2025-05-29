87a7982ae6e4eadcc906a8811919fc992953a4fa
SIDDHARTH SINGH <100992573+SiddharthSingh022@users.noreply.github.com>
2025-05-29 14:38:05 +0530

    q-9.cpp

--- q-9.cpp
+++ q-9.cpp
@@ -2,22 +2,22 @@
 #include <iostream>
 using namespace std;
 void printFibonacci(int n) {
-    int a = 0, b = 1;
-    cout << "Fibonacci Series up to " << n << " terms: ";
-   for (int i = 0; i < n; ++i) {
-        cout << a << " ";
-        int next = a + b;
-        a = b;
-        b = next;
+    int a=0, b=1;
+    cout<<"Fibonacci Series up to"<<n<<"terms:";
+   for (int i = 0; i<n;++i) {
+        cout<<a<<" ";
+        int next=a+b;
+        a=b;
+        b=next;
     }
-    cout << endl;
+    cout<<endl;
 }
 int main() {
     int terms;
-    cout << "Enter the number of terms: ";
-    cin >> terms;
-    if (terms <= 0)
-        cout << "Please enter a positive number." << endl;
+    cout<<"Enter the number of terms:";
+    cin>>terms;
+    if(terms<= 0)
+        cout<<"Please enter a positive number."<<endl;
     else
         printFibonacci(terms);
     return 0;
